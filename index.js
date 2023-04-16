ad83e9ff0f7f770df771bfe598a59f7c62d94849
John Kenneth <johnkennethagpaoa@gmail.com>
2023-04-16 14:30:00 +0800

    added event listeners to buttons

---
+++ index.js
@@ -0,0 +1,7 @@
+drums = document.querySelectorAll(".drum");
+
+for (var i = 0; i < drums.length; i++) {
+    drums[i].addEventListener("click", function () {
+        alert("I got clicked!");
+    });
+}
