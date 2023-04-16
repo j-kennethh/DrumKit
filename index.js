ef671f1e1e2b9a70fe56b6a67bc0bf68b1584a4e
John Kenneth <johnkennethagpaoa@gmail.com>
2023-04-16 14:48:46 +0800

    added drum sounds

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ drums = document.querySelectorAll(".drum");
 
 for (var i = 0; i < drums.length; i++) {
     drums[i].addEventListener("click", function () {
-        alert("I got clicked!");
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
     });
 }
